7f9ac5b8778f9f957a44d3c6f1eae12e56a28ae6
Nida Imran <nimran@pdx.edu>
2017-01-21 23:52:57 -0800

    updated loog

--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -4,20 +4,30 @@ import { NavController } from 'ionic-angular';
 
 import { Storage } from '@ionic/storage';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 declare var window: any;
+
 
 @Component({
   selector: 'page-page2',
   templateUrl: 'page2.html'
 })
+
 export class Page2 {
-
-  constructor(public navCtrl: NavController, public storage: Storage) {
-
+  posts: any;
+ 
+  constructor(public http: Http) {
+ 
+    this.posts = null;
+ 
+this.http.get('https://www.reddit.com/r/gifs/top/.json?limit=2&sort=hot').map(res => res.json()).subscribe(data => {
+    this.posts = data.data.children;
+    console.log(this.posts);
+});
+    
+ 
   }
-  
-    public open(url) {
-		window.cordova.InAppBrowser.open(url, "_self", "location=no");
-	}
 
 }
