39233e509d865d02f5a147fcca8015666985e0be
TUAN LAM <tuanlam.516@gmail.com>
2017-02-06 15:45:47 -0800

    Save Button, <form> and onSubmit() added by Teal, added new variables for Toggles, working on function to grab current Toggle state for storage. Temp states are currently used

--- src/pages/page4/page4.ts
+++ src/pages/page4/page4.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, ToastController } from 'ionic-angular';
 
 import { Storage } from '@ionic/storage';
 
@@ -14,19 +14,56 @@ import { Vibration } from 'ionic-native';
 export class Page4 {
   username: string;
   access_token: string;
+  notification_enabled: boolean;
+  vibration_enabled: boolean;
 
-  constructor(public navCtrl: NavController, public storage: Storage, public alertCtrl: AlertController) {
-	this.storage.get('username').then((value) => {
-		this.username = value;
-	});
+  constructor(
+    public navCtrl: NavController,
+    public storage: Storage,
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController) {
 
-	this.storage.get('access_token').then((value) => {
-		this.access_token = value;
-	});
+    this.storage.get('username').then((value) => {
+		  this.username = value;
+	  });
+
+    this.storage.get('access_token').then((value) => {
+		  this.access_token = value;
+	  });
+
+    this.storage.get ('notification_enabled').then((value) => {
+	    this.notification_enabled = value;
+    });
+
+	  this.storage.get('vibration_enabled').then((value) => {
+	    this.vibration_enabled = value;
+    });
   }
 
   onSubmit(formData) {
+    // the second parameter needs to get the actual value being set when the user changes it.
+    // something like:
+    // this.storage.set('notification_enabled', currentEnabledState('notification_enabled'));
+    this.storage.set('notification_enabled', false);
+    this.storage.set('vibration_enabled', true);
+  }
 
+
+  currentEnabledState(name) {
+    // I'm thinking if we just use a function to pass in the variable we need to look up and return the boolean value
+  }
+
+  // this function will be called when the state of the toggle changes and presents a toast to notify the change
+  vibrateToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Vibrate Enabled',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+    toast.present();
   }
 
   showAbout() {
