8edd64bbb71ae72f913a1dc3a7ce6967707f740c
earthshine0 <earthshinelynx@gmail.com>
2017-02-23 12:45:41 -0800

    updated url to follow a uniform convention

--- src/providers/beeminder-api.ts
+++ src/providers/beeminder-api.ts
@@ -47,7 +47,7 @@ export class BeeminderApi {
   }
 
   editGoal(goal) {
-    let url = `${this.baseUrl}/users/me/goals/${goal.slug}.json?access_token${this.access_token}`;
+    let url = `${this.baseUrl}/users/me/goals/${goal.slug}.json?access_token=${this.access_token}`;
     return this.http.put(url, goal)
              .map(res => res.json())
              .catch(err => Observable.throw(err.json().error));
@@ -61,7 +61,7 @@ export class BeeminderApi {
   }
 
   fetchDatapoints(slug) {
-    let url = '${this.baseUrl}/users/me/goals/${slug}/datapoints.json?access_token=' + this.access_token;
+    let url = this.baseUrl + '/users/me/goals/${slug}/datapoints.json?access_token=' + this.access_token;
     return this.http.get(url)
       .map(res => res.json())
       .catch(err => Observable.throw(err.json().error));
