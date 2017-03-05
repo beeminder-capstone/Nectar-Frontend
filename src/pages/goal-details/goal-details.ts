f58acc7c08377c205ad7814a953e4a878f985789
earthshine0 <earthshinelynx@gmail.com>
2017-03-05 15:10:20 -0800

    formatting goal details page- adding popover menu

--- src/pages/goal-details/goal-details.ts
+++ src/pages/goal-details/goal-details.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {NavController, NavParams, PopoverController, ViewController} from 'ionic-angular';
 import { User } from '../../providers/user';
-
 import { EditGoalPage } from '../edit-goal/edit-goal';
+
+@Component({
+  template: `
+    <ion-list>
+      <button ion-button (click)="editSettingsTapped($event)">Edit Settings</button>
+    </ion-list>
+  `
+})
+
+export class PopoverPage {
+  constructor(public viewCtrl: ViewController, public navCtrl: NavController, public goal:{}) {}
+
+  close() {
+    this.viewCtrl.dismiss();
+  }
+  editSettingsTapped(event) {
+    this.navCtrl.push(EditGoalPage, this.goal);
+  }
+}
+
 
 @Component({
   selector: 'page-goal-details',
@@ -14,12 +33,12 @@ export class GoalDetailsPage {
   showUpdateComponent: boolean = false;
   datapoints = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private user: User) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private user: User, private popoverCtrl: PopoverController) {
 
 
   }
 
-ionViewDidLoad() {
+  ionViewDidLoad() {
     this.goal = this.navParams.data;
     console.log(this.goal);
     this.user.getDatapoints(this.goal).subscribe((data) =>{
@@ -29,5 +48,13 @@ ionViewDidLoad() {
 
   editSettingsTapped(event) {
     this.navCtrl.push(EditGoalPage, this.goal);
+  }
+
+  presentPopover(ev) {
+
+    let popover = this.popoverCtrl.create(PopoverPage,this.goal);
+    popover.present({
+      ev: ev
+    });
   }
 }
