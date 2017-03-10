701660ddf702fe737ae2e37262075e19fdd24dbb
earthshine0 <earthshinelynx@gmail.com>
2017-03-10 11:54:00 -0800

    Fixed home.html and home.ts bugs Reformatted files to match proper html syntax

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,35 +1,31 @@
-import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-import { NavController, NavParams, MenuController, ToastController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {Http} from '@angular/http';
+import {NavController, NavParams, MenuController, ToastController} from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
-import { User } from './../../providers/user';
-import { GoalDetailsPage } from '../goal-details/goal-details';
+import {User} from './../../providers/user';
+import {GoalDetailsPage} from '../goal-details/goal-details';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  selectedItem: any;
   icons: string[];
-  public goals: {}; 
-  items: Array<{ lastUpdate: Date, name: string, laneColor: string, icon: any, }>;
+  public goals: {};
 
   constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams, public menu: MenuController, private user: User) {
 
     this.menu.swipeEnable(true);
     user.getGoals().subscribe((goals) => {
-      this.items = new Array();
-        this.goals = goals;
+      this.goals = goals;
       for (let goal of goals) {
-          goal.lastUpdated = new Date(goal.updated_at * 1000), 
+        goal.lastUpdated = new Date(goal.updated_at * 1000),
           goal.laneColor = this.laneColorFunc(goal.lane),
-          goal.icon = goal.autodata==null? "assets/logos/beeminder.png" : "assets/logos/" + goal.autodata + ".png"
+          goal.icon = goal.autodata == null ? "assets/logos/beeminder.png" : "assets/logos/" + goal.autodata + ".png"
       }
 
     });
-
   }
 
 //   presentToast() {
@@ -53,17 +49,17 @@ export class HomePage {
   }
 
   laneColorFunc(laneLevel) {
-    if(laneLevel >= 2){
-        return "ontrack";
+    if (laneLevel >= 2) {
+      return "ontrack";
     }
-    if(laneLevel == 1){
-        return "good";
+    if (laneLevel == 1) {
+      return "good";
     }
-    else if(laneLevel == -1){
-        return "trouble";
+    else if (laneLevel == -1) {
+      return "trouble";
     }
-    else{
-        return "offtrack";
+    else {
+      return "offtrack";
     }
   }
 
