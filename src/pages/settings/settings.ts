68972e54f7cdcac8f5c5a3a57a3ade7308b5107d
naxt25 <nate.axt@gmail.com>
2017-03-12 16:14:14 -0700

    Added license headers to all files (#20)

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,3 +1,9 @@
+/**
+ * Created by PSU Beeminder Capstone Team on 3/12/2017.
+ * Copyright (c) 2017 PSU Beeminder Capstone Team
+ * This code is available under the "MIT License".
+ * Please see the file LICENSE in this distribution for license terms.
+ */
 import { Component } from '@angular/core';
 
 import { AlertController, ToastController } from 'ionic-angular';
