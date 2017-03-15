4ede5d5de58cc5f1edaa603b84f60b65766340d8
earthshine0 <earthshinelynx@gmail.com>
2017-03-14 23:26:19 -0700

    adjusted parameter for datasource

--- src/pages/create-goal-settings/create-goal-settings.ts
+++ src/pages/create-goal-settings/create-goal-settings.ts
@@ -22,7 +22,7 @@ export class CreateGoalSettingsPage {
 	constructor(public navCtrl: NavController, public storage: Storage, private params: NavParams, public user: User, private toastCtrl: ToastController ) {
     this.metricParam = params.get("metric");
     this.manualGoalParam = params.get("manualGoal");
-    this.integrationParam = this.manualGoalParam==true ? "Manual" : params.get("integration");
+    this.integrationParam = this.manualGoalParam==true ? "manual" : params.get("integration");
     this.icon = this.manualGoalParam==true ? "assets/logos/nectar.png" : "assets/logos/" + this.integrationParam + ".png"
   }
 
@@ -47,7 +47,7 @@ export class CreateGoalSettingsPage {
       title: formData.goalName,
       goaldate: goaldate,
       goal_type: "hustler",
-      datasource: "api",
+      datasource: this.integrationParam,
       rate: formData.rate,
       gunit: formData.gunit,
       runit: formData.runit
